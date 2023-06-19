df342d7922ce09187c357a3fa36e476323d95ab4
yiwangtz <yiwangtz@gmail.com>
2023-06-20 10:02:21 +1200

    menet

---
+++ dataset.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+#coding=utf-8
+
+import os
+import cv2
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+########################### Data Augmentation ###########################
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean 
+        self.std  = std
+    
+    def __call__(self, image, mask=None, gradient=None):
+        image = (image - self.mean)/self.std
+        if mask is None:
+            return image
+        return image, mask/255, gradient/255
+
+class RandomCrop(object):
+    def __call__(self, image, mask=None, gradient=None):
+        H,W,_   = image.shape
+        randw   = np.random.randint(W/8)
+        randh   = np.random.randint(H/8)
+        offseth = 0 if randh == 0 else np.random.randint(randh)
+        offsetw = 0 if randw == 0 else np.random.randint(randw)
+        p0, p1, p2, p3 = offseth, H+offseth-randh, offsetw, W+offsetw-randw
+        if mask is None:
+            return image[p0:p1,p2:p3, :]
+        return image[p0:p1,p2:p3, :], mask[p0:p1,p2:p3], gradient[p0:p1,p2:p3]
+
+class RandomFlip(object):
+    def __call__(self, image, mask=None, gradient=None):
+        if np.random.randint(2)==0:
+            if mask is None:
+                return image[:,::-1,:].copy()
+            return image[:,::-1,:].copy(), mask[:, ::-1].copy(), gradient[:, ::-1].copy()
+        else:
+            if mask is None:
+                return image
+            return image, mask, gradient
+
+class Resize(object):
+    def __init__(self, H, W):
+        self.H = H
+        self.W = W
+
+    def __call__(self, image, mask=None, gradient=None):
+        image = cv2.resize(image, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR)
+        if mask is None:
+            return image
+        mask  = cv2.resize( mask, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR)
+        gradient  = cv2.resize( gradient, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR)
+        return image, mask, gradient
+
+class ToTensor(object):
+    def __call__(self, image, mask=None, gradient=None):
+        image = torch.from_numpy(image)
+        image = image.permute(2, 0, 1)
+        if mask is None:
+            return image
+        mask  = torch.from_numpy(mask)
+        gradient  = torch.from_numpy(gradient)
+        return image, mask, gradient
+
+
+########################### Config File ###########################
+class Config(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.mean   = np.array([[[124.55, 118.90, 102.94]]])
+        self.std    = np.array([[[ 56.77,  55.97,  57.50]]])
+        print('\nParameters...')
+        for k, v in self.kwargs.items():
+            print('%-10s: %s'%(k, v))
+
+    def __getattr__(self, name):
+        if name in self.kwargs:
+            return self.kwargs[name]
+        else:
+            return None
+
+
+########################### Dataset Class ###########################
+class Data(Dataset):
+    def __init__(self, cfg):
+        self.cfg        = cfg
+        self.normalize  = Normalize(mean=cfg.mean, std=cfg.std)
+        self.randomcrop = RandomCrop()
+        self.randomflip = RandomFlip()
+        self.resize     = Resize(352, 352)
+        self.totensor   = ToTensor()
+
+        with open(cfg.datapath+'/train.txt', 'r') as lines:
+            self.samples = []
+            for line in lines:
+                self.samples.append(line.strip())
+
+    def __getitem__(self, idx):
+        name  = self.samples[idx]
+        try:
+            image = cv2.imread(self.cfg.datapath+'/image/'+name+'.jpg')[:,:,::-1].astype(np.float32)
+        except:
+            print('error name:', name)
+
+        if self.cfg.mode=='train':
+            mask  = cv2.imread(self.cfg.datapath+'/mask/' +name+'.png', 0).astype(np.float32)
+            gradient = cv2.imread(self.cfg.datapath+'/gradient/' +name+'.png', 0).astype(np.float32)
+            image, mask, gradient = self.normalize(image, mask, gradient)
+            image, mask, gradient = self.randomcrop(image, mask, gradient)
+            image, mask, gradient = self.randomflip(image, mask, gradient)
+            return image, mask, gradient
+        else:
+            shape = image.shape[:2]
+            image = self.normalize(image)
+            image = self.resize(image)
+            image = self.totensor(image)
+            return image, shape, name
+
+    def __len__(self):
+        return len(self.samples)
+    
+    def collate(self, batch):
+        size = [224, 256, 288, 320, 352][np.random.randint(0, 5)]
+        image, mask, gradient = [list(item) for item in zip(*batch)]
+        for i in range(len(batch)):
+            image[i] = cv2.resize(image[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR)
+            mask[i]  = cv2.resize(mask[i],  dsize=(size, size), interpolation=cv2.INTER_LINEAR)
+            gradient[i] = cv2.resize(gradient[i],  dsize=(size, size), interpolation=cv2.INTER_LINEAR)
+        image  = torch.from_numpy(np.stack(image, axis=0)).permute(0,3,1,2)
+        mask   = torch.from_numpy(np.stack(mask, axis=0)).unsqueeze(1)
+        gradient   = torch.from_numpy(np.stack(gradient, axis=0)).unsqueeze(1)
+        return image, mask, gradient
